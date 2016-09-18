d42546a4772b5e31c49b820df747e13d5d3bf086
Dario Meloni <mellon85@gmail.com>
2016-09-18 13:20:57 +0200

    Added journal mode for sqlite3

--- src/DB.hs
+++ src/DB.hs
@@ -21,7 +21,10 @@ type Connection = HSD.Connection
 
 -- |Connect to the underlying SQL database
 connect :: FilePath -> IO HSD.Connection
-connect = HSD.connectSqlite3
+connect x = do
+    c <- HSD.connectSqlite3 x
+    HS.quickQuery c "PRAGMA journal_mode=WAL" []
+    return c
 
 disconnect :: (HS.IConnection a) => a -> IO ()
 disconnect = HS.disconnect
