4d27f19f07b943b70346327c49b07cb3890a9e7b
Dario Meloni <mellon85@gmail.com>
2016-12-17 01:30:06 +0100

    findDiffs tests completed

--- test/FSWatcherTest.hs
+++ test/FSWatcherTest.hs
@@ -6,40 +6,14 @@ import Data.Conduit
 import Data.Conduit.List hiding(head, take, drop)
 import Test.QuickCheck
 import Test.QuickCheck.Monadic
+import Test.QuickCheck.Modifiers
+
 import Data.Time.Clock
-import System.Random
 
 import qualified FSWatcher as FS
 import MyArbitrary
 
-
-randomSplitIO :: [a] -> Int -> IO [[a]]
-randomSplitIO xs 0 = return [xs]
-randomSplitIO xs 1 = return [xs]
-randomSplitIO xs n = do
-    c <- getStdRandom $ randomR (0, length xs -1)
-    next <- randomSplitIO (drop c xs) (n-1)
-    return $ (take c xs) : next
-
 fromPath p t = FS.File p t False
-
-prop_SimpleDiff path1 path2 = path1 /= path2 ==> monadicIO $ do
-    v <- run $ do
-        t <- getCurrentTime
-        s1 <- return $ sourceList [fromPath path1 t]
-        s2 <- return $ sourceList [fromPath path2 t]
-        df <- FS.findDiffs s1 s2 $$ consume
-        return $ length df
-    assert $ v == 2
-
-prop_SimpleDiff_Eq e1 = monadicIO $ do
-    v <- run $ do
-        t <- getCurrentTime
-        s1 <- return $ sourceList e1
-        s2 <- return $ sourceList e1
-        df <- FS.findDiffs s1 s2 $$ consume
-        return $ length df
-    assert $ v == 0
 
 allDiff [] = True
 allDiff [_] = True
@@ -47,14 +21,17 @@ allDiff (a:xs) | a /= (head xs) &&
                  FS.entryPath a /= FS.entryPath (head xs) = allDiff xs
                | otherwise      = False
 
-prop_diff ex = allDiff ex ==> monadicIO $ do
-    v <- run $ do
-        [ex1, ex2] <- randomSplitIO ex 2
-        s1 <- return $ sourceList ex1
-        s2 <- return $ sourceList ex2
+prop_findDiffs_correct (Ordered ex) (Positive a) (Positive b) = allDiff ex &&
+            a >= 0 && a < b && b < length ex ==> monadicIO $ do
+    (v,t) <- run $ do
+        ex1 <- return $ take a ex
+        ex2 <- return $ take (a-b) $ drop a ex
+        common <- return $ drop b ex
+        s1 <- return $ sourceList $ ex1 ++ common
+        s2 <- return $ sourceList $ ex2 ++ common
         df <- FS.findDiffs s1 s2 $$ consume
-        return $ length df
-    assert $ v == length ex
+        return $ (length df, length ex1+length ex2)
+    assert $ v == t
 
 return []
 runTests = $quickCheckAll
