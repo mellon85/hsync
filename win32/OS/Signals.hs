88c0d97a0f769c9334f6d7239be4024f6ee2fb2c
Dario Meloni <mellon85@gmail.com>
2017-02-21 22:18:41 +0100

    Experimental Win32 event

--- win32/OS/Signals.hs
+++ win32/OS/Signals.hs
@@ -1,3 +1,20 @@
-module OS.Signals_internal
+module OS.Signals
+    (installSignalHandlers)
     where
+
+import Foreign.Ptr
+import System.Win32.Console.CtrlHandler
+
+foreign import ccall "wrapper"
+    mkSignalHandler :: Handler -> IO PHANDLER_ROUTINE
+
+signalWrapper :: IO () -> Handler
+signalWrapper signal _ = do -- we ignore the event
+    signal
+    return True
+
+installSignalHandlers :: IO () -> IO ()
+installSignalHandlers signal = do
+    c_SetConsoleCtrlHandler (mkSignalHandler $ signalWrapper signal) True
+    return ()
 
